e6653b5a56b343f1a8af11759723e0be972e7213
siddhantprateek <siddhantprateek@gmail.com>
2022-03-04 16:31:44 +0530

    Updates

--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as TechList } from './tech-list/techlist.component'
-export { default as TechCard } from './tech-card/techcard.components'
--- src/routes/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from './home/home.pages';
-export { default as About } from './about/about.pages';
-export { default as Projects } from './projects/project.pages';
-export { default as Experience } from './experience/experience.pages';
-export { default as Blogs } from './blogs/blogs.pages';
