381ee1f4e132ea1d8832f5611b6eaff56dbcd082
siddhantprateek <siddhantprateek@gmail.com>
2022-04-12 09:53:35 +0530

    🚩experience section added hackathon and community

--- src/routes/experience/experience.pages.jsx
+++ src/routes/experience/experience.pages.jsx
@@ -1,6 +1,14 @@
 
 import React from 'react';
 import './experience.styles.css';
+
+const NoData = () => {
+    return(
+        <div className="nodata">
+            <p>No Data Added</p>
+        </div>
+    )
+}
 class Experience extends React.Component{
     render(){
         return(
@@ -9,7 +17,17 @@ class Experience extends React.Component{
                     <h1 className='exp-head-back'>Places Where I have worked...</h1>
                     <h1 className='exp-head-front'>Experiences</h1>
                 </div>
-                
+                <NoData/>
+                <div className='exp-div'>
+                    <h1 className='exp-head-back'>Hackathons</h1>
+                    <h1 className='exp-head-front'>Hackathons</h1>
+                </div>
+                <NoData/>
+                <div className='exp-div'>
+                    <h1 className='exp-head-back'>Worked with</h1>
+                    <h1 className='exp-head-front'>Communities</h1>
+                </div>
+                <NoData/>
             </div>
         )
     }
