f0daf4aad77a8f9a61f0083d802d557742db5ec2
siddhantprateek <siddhantprateek@gmail.com>
2022-04-12 09:51:28 +0530

    🚩tech img fixed height

--- src/components/tech-card/techcard.components.jsx
+++ src/components/tech-card/techcard.components.jsx
@@ -3,7 +3,9 @@ const TechCard = ({ tech, url }) => {
   return (
     <div className="tech-box">
       <div className="tech-card-content">
-        <img src={url} alt="" />
+        <div className="tech-img-container">
+          <img src={url} alt="" />
+        </div>
         <p>{tech}</p>
       </div>
     </div>
